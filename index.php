5d996170ce8dfabc2930c673de510203af046288
Lubomir Stankov <lubomir.c.stankov@gmail.com>
2019-12-22 14:12:31 -0500

    Change plugin version number

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ Plugin Name: Lyrics Ranking
 Plugin URI: https://ranking.lyrics.bg
 Description: Custom plugin for ranking made for lyrics.bg
 Author: Lubomir Stankov
-Version: 1.0.12
+Version: 1.0.13
 Author URI: https://lstankov.me/
 */
-!defined('LyricsRanking_VER') ? define('LyricsRanking_VER','1.0.12') : LyricsRanking_VER;
+!defined('LyricsRanking_VER') ? define('LyricsRanking_VER','1.0.13') : LyricsRanking_VER;
 
 
 /*
