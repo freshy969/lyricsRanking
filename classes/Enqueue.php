8691e847d5eb1f0958ffd7fab750811cb219b3ba
Lubomir Stankov <lubomir.c.stankov@gmail.com>
2019-12-22 10:45:58 -0500

    Add description to Enqueue class

--- classes/Enqueue.php
+++ classes/Enqueue.php
@@ -2,7 +2,9 @@
 namespace Lubomir\LyricsRanking;
 
 class  Enqueue {
-
+	/* 
+	 * Page scripts
+	 */
 	public function __construct() {
 		wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' );
 		wp_enqueue_script(array('jquery'));
